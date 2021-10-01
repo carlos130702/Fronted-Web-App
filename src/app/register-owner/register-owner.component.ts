c70bb3e4a316fc4273b1cfc028442123e848e745
EdwarZevallos <place4breathing@gmail.com>
2021-09-30 20:53:42 -0500

    add: register-owner

--- src/app/register-owner/register-owner.component.ts
+++ src/app/register-owner/register-owner.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from "@angular/forms"; "@angular/forms";
 
 @Component({
   selector: 'app-register-owner',
   templateUrl: './register-owner.component.html',
   styleUrls: ['./register-owner.component.css']
 })
+
 export class RegisterOwnerComponent implements OnInit {
 
-  constructor() { }
+  submitted: boolean=false;
+
+  registerForm: FormGroup = this.formBuilder.group({
+    ownerName: ['', {validators: [Validators.required], undateOn: 'change'}],
+    ownerLastName: ['', {validators: [Validators.required], undateOn: 'change'}],
+    ownerEmail: ['',{validators: [Validators.required, Validators.email], updateOn: 'change'}],
+    ownerphone: ['',{update:'change'}],
+    password: ['', {validators: [Validators.required, Validators.minLength(5)], updatedOn:'change'}],
+  });
+
+
+  constructor( private formBuilder: FormBuilder) { }
+
+
 
   ngOnInit(): void {
+    this.setPhoneValidation();
+  }  
+  
+  //Properties
+  get ownerName(){ return this.registerForm.get('ownerName');}
+  get ownerLastName(){ return this.registerForm.get('ownerLastName');}
+  get ownerEmail(){ return this.registerForm.get('ownerEmail');}
+  get ownerPhone(){ return this.registerForm.get('ownerPhone');}
+  get password(){ return this.registerForm.get('password');}
+  get passwordRepeat(){ return this.registerForm.get('passwordRepeat');}
+
+  //Dynamic validation setup
+  setPhoneValidation(){
+    const phoneControl = this.registerForm.get('ownerPhone');
+    //Default Validations
+    phoneControl?.setValidators([Validators.pattern('^[0-9]*$'), Validators.required]);
+  }
+
+  submitForm(){
+    console.log(this.registerForm.valid);
+    this.submitted = true;
   }
 
 }
